17e3def9f3b3c1c1c9d84a13a51c05856f0c7ff4
victorcamposleal <camposlealvictor@gmail.com>
2020-05-22 00:23:31 +0200

    modulo1 bootcmap first commit

---
+++ nota pelicula/Nota Película/index.js
@@ -0,0 +1,40 @@
+let pelicula=window.prompt('introduce el titilo de una pelicula');
+
+//let puntuacion=parseInt(window.prompt('introduce el titilo de una pelicula'));
+
+
+document.getElementById('titulo').innerHTML=pelicula;
+
+
+
+// let titulo = window.prompt('Introduce el título de la película');
+
+// let nota;
+
+// do {
+
+//     nota = parseInt(window.prompt('Introduce la nota de la película'));
+
+// } while(nota < 0 || nota > 5 || isNaN(nota));
+
+// document.getElementById('titulo').innerHTML = titulo;
+
+// ​
+
+// let texto = '';
+
+// for(let i = 1; i <= 5; i++) {
+
+//     if(i <= nota) {
+
+//         texto += '<img src="signs.svg" />';
+
+//     } else {
+
+//         texto += '<img src="shapes-and-symbols.svg" />';
+
+//     }
+
+// }
+
+// document.getElementById('nota').innerHTML = texto;
